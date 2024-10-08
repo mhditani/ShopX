f7554d9e6033d6afb0f03a4cb9e0a90f0fd99818
mohamad itani <mohammad.adnan.itani@gmail.com>
2024-10-08 14:55:30 +0300

    Add project files.

---
+++ ShopX_API/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopX_API.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [MaxLength(100)]
+
+        public string LastName { get; set; } = "";
+        [MaxLength(100), EmailAddress]
+
+        public string Email { get; set; } = "";
+        [MaxLength(100)]
+
+        public string Phone { get; set; } = "";
+        [MaxLength(100)]
+
+        public string Subject { get; set; } = "";
+
+        public string Message { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
+    }
+}
---
+++ ShopX_API/Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopX_API.Models;
+
+namespace ShopX_API.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
